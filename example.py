29c1bb605549dede6b1991824b208eeb9f6e737a
NIKKI HISANZA <hisanzanikki@gmail.com>
2025-11-28 23:34:24 +0800

    Add files via upload

---
+++ example.py
@@ -0,0 +1,10 @@
+git clone https://github.com/hisanzanikki/Hisanza_Basic-Payroll-Computation-System-for-Sari-Sari-Store_4.git
+Hisanza_Basic-Payroll-Computation-System-for-Sari-Sari_4
+>git config --global user.name "hisanzanikki"
+>git config --global user.name "hisanzanikki
+Hisanza_Basic-Payroll-Computation-System-for-Sari-Sari_4>git add .
+Hisanza_Basic-Payroll-Computation-System-for-Sari-Sari_4>git commit --"initial commit example"
+Hisanza_Basic-Payroll-Computation-System-for-Sari-Sari_4
+Hisanza_Basic-Payroll-Computation-System-for-Sari-Sari_4>git branch --main
+Hisanza_Basic-Payroll-Computation-System-for-Sari-Sari_4>git remote add origin https://github.com/hisanzanikki/Hisanza_Basic-Payroll-Computation-System-for-Sari-Sari-Store_4.git
+Hisanza_Basic-Payroll-Computation-System-for-Sari-Sari_4>git push -u origin main
